d03e7545af84d5d557c87b804c29e06a1195108d
NextWork123 <nextdesigns@libero.it>
2022-03-09 21:18:28 +0100

    Alioth: Bring-up Project Elixir

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -8,20 +8,26 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common  Project Elixir stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-TARGET_BOOT_ANIMATION_RES := 108o
-PRODUCT_NAME := nad_alioth
+TARGET_BOOT_ANIMATION_RES := 1080
+PRODUCT_NAME := aosp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
-TARGET_SUPPORTS_QUICK_TAP := true
+# Project Elixir Official Stuff
+Elixir_MAINTAINER := NextWorks 
+IS_PHONE := true
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
